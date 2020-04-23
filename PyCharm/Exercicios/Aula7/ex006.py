bcb013e8ba706d1063f30096468527597ea43312
Fábio Darice da Silva <fdarice@gmail.com>
2020-04-23 15:59:58 -0300

    Update ex006.py

--- PyCharm/Exercicios/Aula7/ex006.py
+++ PyCharm/Exercicios/Aula7/ex006.py
@@ -1,2 +1,2 @@
 n = float(input('Digite um número '))
-print('O número digitado foi {}, o dobro deste número é {}, o triplo é {} e sua raiz quadrada é {}'.format(n, n*2, n*3, n**(1/2)))
+print('O número digitado foi {}, o dobro deste número é {}, o triplo é {} e sua raiz quadrada é {:.2f}'.format(n, n*2, n*3, n**(1/2)))

d74e6183384ca53d54364bb0c8bb75fc5981cf7c
Fábio Darice da Silva <fdarice@gmail.com>
2020-04-23 18:13:14 -0300

    Exercício 14 da aula 7

---
+++ PyCharm/Exercicios/Aula7/ex014.py
@@ -0,0 +1,4 @@
+print('Vamos converter °C em °F')
+c = float(input('Digite o valor em °C '))
+print('O valor é {}°F'.format(((9 * c) / 5) + 32))
+# Utilizado os parênteses no cálculo apenas para ficar mais fácil a leitura do código

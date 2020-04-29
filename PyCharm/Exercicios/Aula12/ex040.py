da5c42a2fa929d15f1d42a83af3f0a1051703a8e
Fábio Darice da Silva <fdarice@gmail.com>
2020-04-29 15:40:09 -0300

    Atualização exercício 40

--- PyCharm/Exercicios/Aula12/ex040.py
+++ PyCharm/Exercicios/Aula12/ex040.py
@@ -1,5 +1,5 @@
 # Importação de bibliotecas
-
+from math import ceil
 
 # Título do programa
 print('\033[1;34;40mCÁLCULO DE NOTA MÉDIA PARA PASSAR DE ANO\033[m')
@@ -7,15 +7,15 @@ print('\033[1;34;40mCÁLCULO DE NOTA MÉDIA PARA PASSAR DE ANO\033[m')
 # Objetos
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
-media = (nota1 + nota2) / 2
+media = ceil((nota1 + nota2) / 2)
 corte1 = 5
-corte2 = 6.9
-corte3 = 7
+corte2 = 7
 
 # Lógica
+print(media)
 if media < corte1:
     print('Sua média foi de \033[1;33m{:.1f}\033[m, você foi \033[1;34mREPROVADO!\033[m'.format(media))
-elif media >= corte1 and media <= corte2:
+elif corte2 > media >= corte1:
     print('Sua média foi de \033[1;33m{:.1f}\033[m, você ficou de \033[1;34mRECUPERAÇÃO!\033[m'.format(media))
-elif media >= corte3:
+elif media >= corte2:
     print('Sua média foi de \033[1;33m{:.1f}\033[m, você foi \033[1;34mAPROVADO!\033[m'.format(media))

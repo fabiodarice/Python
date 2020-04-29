4ecfde240512c4dffda155ba7155d5e6065d0cd5
Fábio Darice da Silva <fdarice@gmail.com>
2020-04-29 14:40:00 -0300

    Exercício aula 12

--- PyCharm/Exercicios/Aula12/ex038.py
+++ PyCharm/Exercicios/Aula12/ex038.py
@@ -8,7 +8,7 @@ print('\033[1;34;40mCOMPARAÇÃO ENTRE DOIS NÚMEROS INTEIROS\033[m')
 n1 = int(input('\033[30mDigite o primeiro número inteiro:\033[m '))
 n2 = int(input('\033[30mDigite o segundo número inteiro:\033[m '))
 
-# Lógica1
+# Lógica
 if n1 > n2:
     print('\033[1;33m-\033[m O \033[1;33mprimeiro valor\033[m é \033[1;34mmaior\033[m')
 elif n1 < n2:

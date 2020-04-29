920588719d85265d1c72c1db63a3482b833720c3
Fábio Darice da Silva <fdarice@gmail.com>
2020-04-29 16:17:45 -0300

    Atualização exercício 43

--- PyCharm/Exercicios/Aula12/ex043.py
+++ PyCharm/Exercicios/Aula12/ex043.py
@@ -15,12 +15,12 @@ obesidade = 40
 
 # Lógica
 if imc < abaixo:
-    print('Seu IMC é \033[4;34m{:.2f}\033[m, você está \033[1;31mABAIXO\033[m do peso!'.format(imc))
+    print('Seu IMC é \033[4;34m{:.1f}\033[m, você está \033[1;31mABAIXO\033[m do peso!'.format(imc))
 elif imc >= abaixo and imc <= ideal:
-    print('Seu IMC é \033[4;34m{:.2f}\033[m, você está com o peso \033[1;32mIDEAL!\033[m'.format(imc))
+    print('Seu IMC é \033[4;34m{:.1f}\033[m, você está com o peso \033[1;32mIDEAL!\033[m'.format(imc))
 elif imc > ideal and imc <= sobrepeso:
-    print('Seu IMC é \033[4;34m{:.2f}\033[m, você está com \033[1;31mSOBREPESO!\033[m'.format(imc))
+    print('Seu IMC é \033[4;34m{:.1f}\033[m, você está com \033[1;31mSOBREPESO!\033[m'.format(imc))
 elif imc > sobrepeso and imc <= obesidade:
-    print('Seu IMC é \033[4;34m{:.2f}\033[m, você está com \033[1;31mOBESIDADE!\033[m'.format(imc))
+    print('Seu IMC é \033[4;34m{:.1f}\033[m, você está com \033[1;31mOBESIDADE!\033[m'.format(imc))
 elif imc > obesidade:
-    print('Seu IMC é \033[4;34m{:.2f}\033[m, você está com \033[1;31mOBESIDADE MÓRBIDA!\033[m'.format(imc))
+    print('Seu IMC é \033[4;34m{:.1f}\033[m, você está com \033[1;31mOBESIDADE MÓRBIDA!\033[m'.format(imc))

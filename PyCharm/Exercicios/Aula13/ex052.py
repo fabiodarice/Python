ae5509616fc4c555f4edbcc2be5dbacbd0e50668
Fábio Darice da Silva <fdarice@gmail.com>
2020-05-01 17:30:56 -0300

    Melhoria ex052

--- PyCharm/Exercicios/Aula13/ex052.py
+++ PyCharm/Exercicios/Aula13/ex052.py
@@ -6,16 +6,17 @@ print('\033[1;34;40mVERIFICAÇÃO DE NÚMERO PRIMO\033[m')
 
 # Objetos
 n = int(input('\033[30mDigite um número:\033[m '))
-s = n
-v = 0
+s = 0
 
 # Lógica
-for c in range(n, 0, -1):
-    s = s - 1
-    if s != 0:
-        if s != 1 and n % s == 0:
-            v = 1
-if v == 1:
-    print('Este número \033[1;31mNÃO é primo!!!\033[m')
+for c in range(1, n + 1):
+    if n % c == 0:
+        print('\033[34m', end=' ')
+        s = s + 1
+    else:
+        print('\033[30m', end=' ')
+    print('{}'.format(c), end=' ')
+if s == 2:
+    print('\n\033[mEste número \033[4;34mé primo!!!\033[m')
 else:
-    print('Este número \033[4;34mé primo!!!\033[m')
+    print('\n\033[mEste número \033[1;31mNÃO é primo!!!\033[m')

5f4b6ecdbcc0347e4d6b7f8be8c4c491da3844d9
Fábio Darice da Silva <fdarice@gmail.com>
2020-05-01 16:40:56 -0300

    Melhoria ex051

--- PyCharm/Exercicios/Aula13/ex051.py
+++ PyCharm/Exercicios/Aula13/ex051.py
@@ -7,9 +7,9 @@ print('\033[mPROGRESSÃO ARITMÉTICA\033[m')
 # Objetos
 n = int(input('\033[30mDigite o número do primeiro termo:\033[m '))
 r = int(input('\033[30mDigite a razão da P.A.:\033[m '))
-cor = 0
+cor = 29
 
 # Lógica
-for c in range(n, (10 * r) + 1, r):
-    cor += 1
-    print('\033[33m', c, '\033[m')
+for c in range(n, (n + (10 - 1) * r) + r, r):
+    cor = cor + 1
+    print('\033[{}m'.format(cor), c, '\033[m')

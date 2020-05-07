0acacaeb29f6bbe201f485dcdbb3dadc84bd8a9f
Fábio Darice da Silva <fdarice@gmail.com>
2020-05-07 16:35:50 -0300

    Atualização ex067

--- PyCharm/Exercicios/Aula15/ex067.py
+++ PyCharm/Exercicios/Aula15/ex067.py
@@ -14,15 +14,6 @@ while True:
     print('-' * 30)
     if num < 0:
         break
-    print(f'{num} x {0:2} = {num * 0}\n'
-          f'{num} x {1:2} = {num * 1}\n'
-          f'{num} x {2:2} = {num * 2}\n'
-          f'{num} x {3:2} = {num * 3}\n'
-          f'{num} x {4:2} = {num * 4}\n'
-          f'{num} x {5:2} = {num * 5}\n'
-          f'{num} x {6:2} = {num * 6}\n'
-          f'{num} x {7:2} = {num * 7}\n'
-          f'{num} x {8:2} = {num * 8}\n'
-          f'{num} x {9:2} = {num * 9}\n'
-          f'{num} x {10:2} = {num * 10}')
+    for c in range(0, 11):
+        print(f'{num} x {c:2} = {num * c}')
 print('\033[33mPROGRAMA TABUADA ENCERRADO\033[m, \033[1;34mVolte sempre!\033[m')

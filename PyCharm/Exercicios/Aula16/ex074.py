1e5009b1e5f800c4706b9db23f53a690f0360c51
Fábio Darice da Silva <fdarice@gmail.com>
2020-05-09 18:09:24 -0300

    Atualização ex074

--- PyCharm/Exercicios/Aula16/ex074.py
+++ PyCharm/Exercicios/Aula16/ex074.py
@@ -5,8 +5,7 @@ from random import randint
 print('\033[1;34;40mMAIOR E MENOR VALORES EM TUPLA\033[m')
 
 # Objetos
-maior = 0
-menor = 0
+
 
 # Lógica
 n = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
@@ -14,14 +13,7 @@ n = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 
 print(f'Os valores sorteados foram:', '', end='')
 
 for c in range(0, 5):
-
     print(n[c], '', end='')
 
-    if n[c] > maior:
-        maior = n[c]
-
-    if menor == 0 or n[c] < menor:
-        menor = n[c]
-
-print(f'\nO maior valor sorteado foi {maior}')
-print(f'O manor valor sorteado foi {menor}')
+print(f'\nO maior valor sorteado foi {max(n)}')
+print(f'O manor valor sorteado foi {min(n)}')

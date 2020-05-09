efed7e4eb493cb3d6b9a0a4fe537bcf547778761
Fábio Darice da Silva <fdarice@gmail.com>
2020-05-09 17:44:11 -0300

    Atualização ex072

--- PyCharm/Exercicios/Aula16/ex072.py
+++ PyCharm/Exercicios/Aula16/ex072.py
@@ -8,12 +8,12 @@ print('\033[1;34;40mNÚMERO POR EXTENSO\033[m')
 n = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
      'onze', 'doze', 'treze', 'quartoze', 'quinze', 'dezesseis', 'dezessete', 'dezoito',
      'dezenove', 'vinte')
-num = 0
 
 # Lógica
-num = int(input('\033[30mDigite um número entre 0 e 20:\033[m '))
-
-while num not in range (0, 21):
-    num = int(input('\033[30mTente novamente. Digite um número entre 0 e 20:\033[m '))
+while True:
+    num = int(input('\033[30mDigite um número entre 0 e 20:\033[m '))
+    if 0 <= num <= 20:
+        break
+    print('\033[30mTente novamente.\033[m ', end="")
 
 print(f'Você digitou o número \033[4;34m{n[num]}\033[m')

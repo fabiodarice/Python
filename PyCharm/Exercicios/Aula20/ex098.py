8c430b303051f30958ebeba8e3806a542459591a
Fábio Darice da Silva <fdarice@gmail.com>
2020-05-15 20:48:24 -0300

    Atualização ex098

--- PyCharm/Exercicios/Aula20/ex098.py
+++ PyCharm/Exercicios/Aula20/ex098.py
@@ -8,40 +8,38 @@ print('\033[1;34;40mFUNÇÃO DE CONTADOR\033[m')
 
 
 # Funções
-def contador(inicio, fim, passo):
-    if inicio > fim:
-        fim -= passo
-        passo *= -1
-    else:
-        fim += 1
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    print('-=' * 20)
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(2.5)
 
-    for c in range(inicio, fim, passo):
-        print(f'{c}', '', end='')
-        c += 1
-        sleep(0.2)
-    print('FIM!')
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='')
+            sleep(0.5)
+            cont += p
+        print('FIM!')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='')
+            sleep(0.5)
+            cont -= p
+        print('FIM!')
 
 
 # Lógica
-print('-=' * 30)
-print('Contagem de 1 até 10 de 1 em 1')
+
 contador(1, 10, 1)
-
-print('-=' * 30)
-print('Contagem de 10 até 0 de 2 em 2')
 contador(10, 0, 2)
-
-print('-=' * 30)
+print('-=' * 20)
 print('Agora é sua vez de personalizar a contagem!')
-i = int(input('Início: '))
-f = int(input('Fim: '))
-p = int(input('Passo: '))
-
-if i > f and p < 0:
-    p *= -1
-elif p == 0:
-    p = 1
-
-print('-=' * 30)
-print(f'Contagem de {i} até {f} de {p} em {p}')
-contador(i, f, p)
+ini = int(input('Início: '))
+fim = int(input('Fim: '))
+pas = int(input('Passo: '))
+contador(ini, fim, pas)
